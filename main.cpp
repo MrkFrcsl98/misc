0dc5357cbd446216066191039f82d6c1cb2f9772
Marko <auc_04@proton.me>
2025-03-17 21:09:03 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <math.h>
+#include <chrono>
+#include <string>
+#include <stdint.h>
+
+// 
+
+namespace Math {
+    
+};
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
